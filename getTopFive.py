16c4aa8daf04d2fa3fde09ff94b7ee05ce40fe72
justusdecker <justus.d2025@gmail.com>
2025-02-27 11:49:54 +0100

    Updated getTopFive.py

--- getTopFive.py
+++ getTopFive.py
@@ -5,7 +5,6 @@ Please write something that gets a list of grade in an exam, and returns the top
 ©2025 Justus Decker - getTopFive
 """
 def getTopFive(grades: list | tuple) -> list:
-    grades.sort(), grades.reverse()
-    return grades[:5]
+    return sorted(grades,reverse=True)[:5]
 grades = [14,51,52,63,64,2,52,68,94,34,99,64]
 print(getTopFive(grades))
