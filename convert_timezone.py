63513ab4a71af32ee046a4845cc9b1ec1649646c
justusdecker <justus.d2025@gmail.com>
2025-02-27 13:02:11 +0100

    Update convert_timezone.py

--- convert_timezone.py
+++ convert_timezone.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta,timezone
 
 def changeTimezone(_time,hours:int=4):
-    return datetime.strptime(_time,'%H:%M').astimezone(timezone(timedelta(hours=hours))).strftime("%H:%M")
+    return datetime.strptime(_time,'%H:%M').astimezone(timezone(timedelta(hours=hours + (hours + int(_time[:2]) > 24)))).strftime("%H:%M")
 print(changeTimezone("14:34"))
 print(changeTimezone("22:34"))
